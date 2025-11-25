e0fbf03adc4e85e8e5767aef2d5b3e863a7b767e
Aditya Singh <2389375@cognizant.com>
2025-11-25 16:36:11 +0530

    changed controller

--- src/main/java/com/example/demo/controller/WebController.java
+++ src/main/java/com/example/demo/controller/WebController.java
@@ -8,6 +8,6 @@ public class WebController {
 	
 	@GetMapping("/hello")
 	public String greeting() {
-		return "Hello Spring Boot (Activation)";
+		return "Hello Spring Boot (Activation) Successfull";
 	}
 }
